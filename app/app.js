1d95607ce0725be27b214b5be648d8d59f54a37d
Ashutosh Pradhan <ap5042@att.com>
2017-08-14 19:52:16 +0530

    Commited with basic infastructure of the application.

--- app/app.js
+++ app/app.js
@@ -3,11 +3,23 @@ var angularFirebaseApp = angular.module("angularFirebaseApp", ["ngRoute"]);
 
 angularFirebaseApp.config(function($routeProvider) {
     $routeProvider
-    .when("/main", {
+    .when("/", {
         templateUrl : "/app/views/main.html",
         controller : "mainCtrl",
     })
+    .when("/login", {
+        templateUrl : "/app/views/login.html",
+        controller : "loginCtrl",
+    })
+    .when("/register", {
+        templateUrl : "/app/views/register.html",
+        controller : "regCtrl",
+    })
+    .when("/users", {
+        templateUrl : "/app/views/users.html",
+        controller : "userCtrl",
+    })
     .otherwise({
-        redirectTo: "/main"
+        redirectTo: "/"
     });
 });
---
+++ app/controllers/regCtrl.js
@@ -0,0 +1,8 @@
+'use strict';
+
+angularFirebaseApp.controller('regCtrl', ['$scope','mainSrv', function ($scope, mainSrv) {
+    
+    $scope.currentLocation = "We are currently on Registration Page";
+
+
+}]);
